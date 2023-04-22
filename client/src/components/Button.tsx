df28eccdc185001cdcf3fd4d2f55054b569cd6e4
Andrew Lam <bluey17501@gmail.com>
2023-04-22 19:40:18 +1200

    feat: added contrasting text colour

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -14,6 +14,14 @@ export interface ButtonProps {
   onClick: () => void;
 }
 
+const getTextColor = (hex: string) => {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  return yiq >= 128 ? "#4F4F4F" : "#ffffff";
+};
+
 const Button = ({
   height,
   width = "10rem",
@@ -41,6 +49,7 @@ const Button = ({
           border: backgroundColor,
           borderRadius: "10px",
           padding: "10px",
+          color: getTextColor(backgroundColor),
           fontFamily: "Montserrat",
           fontSize,
           fontWeight,
