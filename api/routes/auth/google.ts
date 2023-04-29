5ce924ddea5e60e81a1f4e45c6be9cd2e3ab2814
Alex McLeod <alexwillmcleod@gmail.com>
2023-04-30 10:33:14 +1200

    feat: moved google auth routes to folder

---
+++ api/routes/auth/google.ts
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+import { google } from 'googleapis';
+
+const router = Router();
+
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_GOOGLE_CLIENT_SECRET;
+const REDIRECT_URI = process.env.REACT_APP_GOOGLE_REDIRECT_URI;
+const SCOPES = [
+  'profile',
+  'email',
+  'https://www.googleapis.com/auth/spreadsheets.readonly',
+];
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+
+router.get('/auth/google', (req, res) => {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'online',
+    scope: SCOPES,
+  });
+
+  res.redirect(authUrl);
+});
+
+router.get('/auth/google/callback', async (req, res) => {
+  const { code } = req.query;
+
+  try {
+    const { tokens } = await oAuth2Client.getToken(code as string);
+    oAuth2Client.setCredentials(tokens);
+    const oauth2 = google.oauth2({ version: 'v2', auth: oAuth2Client });
+    const { data } = await oauth2.userinfo.get();
+    res.json({ user: data, tokens });
+  } catch (error) {
+    console.error('Error retrieving access token', error);
+    res.status(500).send('Error retrieving access token');
+  }
+});
