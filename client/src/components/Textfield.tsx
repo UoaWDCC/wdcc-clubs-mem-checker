2b6ac218cd41a4096a37718c8836c379f24faada
Andrew Lam <bluey17501@gmail.com>
2023-05-02 23:24:48 +1200

    feat: added icons to textfields

--- client/src/components/Textfield.tsx
+++ client/src/components/Textfield.tsx
@@ -1,74 +1,72 @@
-import { getTextColour } from "../utils/helpers";
+import { getTextColor, lightenColor } from "../utils/helpers";
 import styles from "./Textfield.module.css";
-import React, { ForwardedRef, forwardRef } from "react";
+import { forwardRef } from "react";
 
 interface TextfieldProps {
   height?: string;
   width?: string;
-  marginTop?: string;
-  marginBottom?: string;
-  marginLeft?: string;
-  marginRight?: string;
+  margin?: string;
   backgroundColor?: string;
   fontSize?: string;
   fontWeight?: string;
   placeholder?: string;
   label?: string;
-  border?: string;
-  errorText?: string;
   isError?: boolean;
+  icon?: string;
+  iconSize?: string;
   onKeyUp?: () => void;
   onChange?: () => void;
 }
 
-//reusable textfield component
 const Textfield = forwardRef(
   (
     {
       height,
       width = "12rem",
-      marginTop,
-      marginBottom,
-      marginLeft,
-      marginRight,
+      margin,
       backgroundColor = "#e0e0e0",
       fontSize,
       fontWeight,
       placeholder,
       label,
-      border,
-      errorText,
       isError,
+      icon,
+      iconSize = "1rem",
       onKeyUp,
       onChange,
     }: TextfieldProps,
     ref: any
   ) => {
-    if (isError) {
-      placeholder = "Please fill out this field";
-      backgroundColor = "#FFCDD2";
-    }
+    const iconStyles = icon
+      ? {
+          backgroundImage: `url(${icon})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: iconSize,
+          backgroundPosition: "10px",
+          paddingLeft: icon ? `calc(${iconSize} + 1rem)` : "1rem",
+        }
+      : {};
+
     return (
-      <div>
+      <div className={styles.container}>
         <label>{label}</label>
         <input
           ref={ref}
           onKeyUp={onKeyUp}
-          className={isError ? styles.inputError : styles.input}
+          className={styles.input}
           onChange={onChange}
           placeholder={placeholder}
           style={{
             height,
             width,
-            marginTop,
-            marginBottom,
-            marginLeft,
-            marginRight,
+            margin,
             backgroundColor,
-            color: getTextColour(backgroundColor),
-            fontFamily: "Montserrat",
+            color: getTextColor(backgroundColor),
             fontSize,
             fontWeight,
+            border: isError ? "1px solid red" : backgroundColor,
+            ["--focus-border-color" as any]: lightenColor(backgroundColor, -75),
+            ...iconStyles,
           }}
         />
       </div>
