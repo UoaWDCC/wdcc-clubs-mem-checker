fe1475bbed1fe88fd792bfd5e80e822fdc36c37c
Alex McLeod <alexwillmcleod@gmail.com>
2023-05-09 23:57:59 +1200

    feat: created google sign in page and added to router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,26 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { HomePage } from "./pages/home/home";
-import { ExamplePage } from "./pages/example/page";
-import CreateCheckerPage from "./components/create-checker-page/CreateCheckerPage";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { HomePage } from './pages/home/home';
+import { ExamplePage } from './pages/example/page';
+import CreateCheckerPage from './components/create-checker-page/CreateCheckerPage';
+import { GoogleSignIn } from './pages/google-sign-in/GoogleSignInPage';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <HomePage />,
   },
   {
-    path: "/example",
+    path: '/example',
     element: <ExamplePage />,
   },
   {
-    path: "/create-page",
+    path: '/create-page',
     element: <CreateCheckerPage />,
+  },
+  {
+    path: '/sign-in',
+    element: <GoogleSignIn />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <RouterProvider router={router} />
 );
