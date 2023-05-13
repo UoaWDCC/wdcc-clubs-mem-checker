7ebffecdc9e8c65d1300cb7a5e987988e71e5fac
Alex McLeod <alexwillmcleod@gmail.com>
2023-05-13 18:10:45 +1200

    fix: club create endpoint now returns

--- api/routes/club/club.ts
+++ api/routes/club/club.ts
@@ -1,51 +1,43 @@
 import { Router } from 'express';
-import { PrismaClient} from '@prisma/client'
-<<<<<<< HEAD
-=======
+import { PrismaClient } from '@prisma/client';
 import auth from '../../middleware/auth';
->>>>>>> 865ae64637cbf90c5ceeffae344b9988b2e0e71b
 
 const router = Router();
 const prisma = new PrismaClient();
 
-<<<<<<< HEAD
-
-router.get('/:clubName/:clubAcronym', async (req, res) => {
-
-const {clubName, clubAcronym} = req.params;
-const organisation = await prisma.organisation.upsert({
-
-    where: { name: clubName},
-    update: {},
-    create: {
-        name: clubName,
-        acronym: clubAcronym
-    }
-})});
-=======
 router.post('/create', auth, async (req, res) => {
+  const { clubName, clubAcronym } = req.body;
+  const userId = req.body.user.id;
 
-const {clubName, clubAcronym} = req.body;
-const user = req.body.user.id;
+  console.log(
+    `clubName: ${clubName}, clubAcronym: ${clubAcronym}, userId: ${userId}`
+  );
 
-if (!clubName || !clubAcronym) {
-    return res.status(400).send("clubName and clubAcronymn are required body fields");
-}
+  if (!clubName || !clubAcronym) {
+    return res
+      .status(400)
+      .send('clubName and clubAcronymn are required body fields');
+  }
 
-const organisation = await prisma.organisation.create({
+  const organisation = await prisma.organisation.create({
     data: {
-        name: clubName,
-        acronym: clubAcronym
-    }
-})
-await prisma.usersInOrganisation.create({
+      name: clubName,
+      acronym: clubAcronym,
+    },
+  });
+  // console.log(`Created organisation ${organisation}`);
+  const userInOrganisation = await prisma.usersInOrganisation.create({
     data: {
-        userId: user,
-        organisationId: organisation.id
-    }
-    })
-    return res.status(200);
+      userId,
+      organisationId: organisation.id,
+    },
+  });
+  // console.log(`userInOrganisation ${userInOrganisation}`);
+  return res
+    .status(200)
+    .send(
+      `created club ${clubName} with acronym ${clubAcronym} and added user`
+    );
 });
->>>>>>> 865ae64637cbf90c5ceeffae344b9988b2e0e71b
 
 export default router;
