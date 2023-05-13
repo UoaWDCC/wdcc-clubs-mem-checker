26a9ba7c6864175bc6383b7c5f9ecdcd290a84a8
nimito <nimit.parekh1@gmail.com>
2023-05-13 18:16:59 +1200

    fix: updated response to request

--- api/routes/club/club.ts
+++ api/routes/club/club.ts
@@ -1,30 +1,13 @@
 import { Router } from 'express';
 import { PrismaClient} from '@prisma/client'
-<<<<<<< HEAD
-=======
 import auth from '../../middleware/auth';
->>>>>>> 865ae64637cbf90c5ceeffae344b9988b2e0e71b
 
 const router = Router();
 const prisma = new PrismaClient();
 
-<<<<<<< HEAD
-
-router.get('/:clubName/:clubAcronym', async (req, res) => {
-
-const {clubName, clubAcronym} = req.params;
-const organisation = await prisma.organisation.upsert({
-
-    where: { name: clubName},
-    update: {},
-    create: {
-        name: clubName,
-        acronym: clubAcronym
-    }
-})});
-=======
 router.post('/create', auth, async (req, res) => {
 
+console.log(req.body.user.id);
 const {clubName, clubAcronym} = req.body;
 const user = req.body.user.id;
 
@@ -43,9 +26,8 @@ await prisma.usersInOrganisation.create({
         userId: user,
         organisationId: organisation.id
     }
-    })
-    return res.status(200);
+ })
+    return res.status(200).send();
 });
->>>>>>> 865ae64637cbf90c5ceeffae344b9988b2e0e71b
 
 export default router;
