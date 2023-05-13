505d6bfe7f6fe0de43a75e081b709a1bab925d76
nimito <nimit.parekh1@gmail.com>
2023-05-13 16:39:02 +1200

    feat: added user and organisation to usersInOrganisation

--- api/routes/clubendpoint/clubendpoint.ts
+++ api/routes/clubendpoint/clubendpoint.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { PrismaClient} from '@prisma/client'
+import auth from '../../middleware/auth';
 
 const router = Router();
 const prisma = new PrismaClient();
 
+router.get('/:clubName/:clubAcronym', auth, async (req, res) => {
 
-router.get('/:clubName/:clubAcronym', async (req, res) => {
+const user = req.body.user.id; 
 
 const {clubName, clubAcronym} = req.params;
 const organisation = await prisma.organisation.upsert({
@@ -16,6 +18,15 @@ const organisation = await prisma.organisation.upsert({
         name: clubName,
         acronym: clubAcronym
     }
-})});
+})
+await prisma.usersInOrganisation.create({
+    data: {
+        userId: user,
+        organisationId: organisation.id
+    }
+    })
+});
+
+
 
 export default router;
