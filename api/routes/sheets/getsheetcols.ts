938b3df3ac6c27ac8c9602694b9267956d60e7dd
Sebastian Thomas <stho382@aucklanduni.ac.nz>
2023-05-18 09:08:41 +1200

    implemented ability to generate unique column names

--- api/routes/sheets/getsheetcols.ts
+++ api/routes/sheets/getsheetcols.ts
@@ -5,69 +5,6 @@ import {oAuth2Client} from "../auth/google";
 
 export const router = Router();
 
-// router.get('/:spreadsheetId/:sheettabid', auth, async (req, res) => {
-//
-//     oAuth2Client.setCredentials({access_token: req.body.user.googleToken})
-//     const {  spreadsheetId } = req.params;
-//     const {  sheettabid} = req.params;
-//     const sheets = google.sheets({version: 'v4', auth: oAuth2Client});
-//
-//     try {
-//         // Retrieve the metadata for the spreadsheet to get the names and IDs of all sheets
-//         const metadataResponse = await sheets.spreadsheets.get({
-//             spreadsheetId,
-//         });
-//
-//         // If there are no sheets in the spreadsheet, return an empty object
-//         if (!metadataResponse.data.sheets) {
-//             return res.status(400).send(JSON.stringify('Spreadsheet has no sheets.'));
-//         }
-//
-//         // Create an object to store the column data for each sheet
-//         // const sheetData: { [key: string]: { [key: string]: { unique: boolean } } } = {};
-//         const columnData: { [key: string]: { id: string, name: string, unique: boolean } } = {};
-//
-//         // Iterate through each sheet in the spreadsheet
-//         const sheetsData = await Promise.all(metadataResponse.data.sheets.map(async (sheet) => {
-//             const sheetName = sheet.properties!.title;
-//
-//             try {
-//                 // Retrieve the data from the sheet
-//                 const range = `${sheettabid}!A1:Z`;
-//                 const response = await sheets.spreadsheets.values.get({
-//                     spreadsheetId,
-//                     range,
-//                 });
-//
-//                 const sheet = metadataResponse.data.sheets.find((sheet) => sheet.properties.sheetId === Number(sheettabid));
-//
-//                 // Extract the data from the response
-//                 const rows = response.data.values || [];
-//
-//                 // Determine the columns in the sheet and whether each is unique
-//                 rows[0].forEach((header, index) => {
-//                     const column = rows.map((row) => row[index]);
-//                     const isUnique = column.filter((value, i, self) =>
-//                         self.indexOf(value) === i).length === column.length;
-//                     const columnLetter = String.fromCharCode(65 + index);
-//                     columnData[header] = {id: columnLetter, name: header, unique: isUnique};
-//                 });
-//
-//                 return columnData;
-//             } catch (err) {
-//                 console.error(err)
-//                 return res.status(400).send(JSON.stringify(`Error retrieving data from sheet ${sheetName}:`));
-//             }
-//         }));
-//
-//         return res.status(200).send(JSON.stringify(columnData));
-//
-//     } catch (err) {
-//         console.error(err)
-//         return res.status(400).send(JSON.stringify('Error retrieving spreadsheet metadata:'));
-//     }
-// });
-
 router.get('/:spreadsheetId/:sheettabid', auth, async (req, res) => {
     oAuth2Client.setCredentials({access_token: req.body.user.googleToken});
     const {spreadsheetId, sheettabid} = req.params;
