0613cbb4770833a9c1c6178a5273265d35a0787b
nimit <nimit.parekh1@gmail.com>
2023-05-26 11:36:00 +1200

    added weblink to create

--- api/routes/customisations/customise-page.ts
+++ api/routes/customisations/customise-page.ts
@@ -1,16 +1,19 @@
 import express, { Request, Response } from 'express';
 import {PrismaClient, User} from '@prisma/client';
 import { nanoid } from 'nanoid';
+//import page from '../../../client/src/pages/create-checker-page/CreateCheckerPage';
+import { create } from 'ts-node';
 
 const prisma = new PrismaClient();
 const router = express.Router();
+
 
 interface PageCustomization {
     name: string;
     organisationId: number;
     sheetId: string;
     webLink: string;
-    backgroundColor?: string | undefined;
+    backgroundColor?: string;
     textFieldBackgroundColor?: string;
     textColor?: string;
     buttonColor?: string;
@@ -26,31 +29,25 @@ router.post('/create-page', async (req: Request, res: Response) => {
 
         const { name, organisationId, sheetId, webLink, ...rest } = customization;
 
-        const page = await prisma.page.create({
-            data: {
-                name: name,
-                organisationId: organisationId,
-                sheetId: sheetId,
-                webLink: webLink,
-                backgroundColor: rest.backgroundColor,
-                textFieldBackgroundColor: rest.textFieldBackgroundColor,
-                textColor: rest.textColor,
-                buttonColor: rest.buttonColor,
-                headingColor: rest.headingColor,
-                logoLink: rest.logoLink,
-                backgroundImageLink: rest.backgroundImageLink,
-                fontFamily: rest.fontFamily!,
-            },
-        });
-
         const pathId = nanoid(); // Generate random path ID
 
-        // Store the path ID in the database for the created page
-        await prisma.page.update({
-            where: { id: page.id },
-            data: { webLink: pathId},
-        });
-
+        const page = await prisma.page.create({
+            data: {
+              name: name, // or simply name, as they have the same name
+              organisationId: organisationId,
+              sheetId: sheetId,
+              webLink: pathId,
+              backgroundColor: rest.backgroundColor,
+              textFieldBackgroundColor: rest.textFieldBackgroundColor,
+              textColor: rest.textColor,
+              buttonColor: rest.buttonColor,
+              headingColor: rest.headingColor,
+              logoLink: rest.logoLink,
+              backgroundImageLink: rest.backgroundImageLink,
+              fontFamily: rest.fontFamily!,
+            },
+          });
+          
         res.status(200).json({ pathId });
     } catch (error) {
         console.error('Error creating page:', error);
