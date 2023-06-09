a0829cef48bf95c85f80c595c462171007d8ed9f
Alex McLeod <alexwillmcleod@gmail.com>
2023-06-09 17:54:30 +1200

    Revert "Gh23 api documentation"

--- client/src/pages/club-detail-page/ClubDetailForm.tsx
+++ client/src/pages/club-detail-page/ClubDetailForm.tsx
@@ -5,101 +5,113 @@ import Textfield from '../../components/Textfield';
 import Button from '../../components/Button';
 import { ClubDetails } from './ClubDetailPage';
 import axios from 'axios';
-import { useNavigate } from 'react-router';
-import WhiteBackButton from '../../components/WhiteBackButton';
 
 const url = '/club/create'; //temp url
 
 interface ClubDetailFormProps {
-	onNext: (clubDetails: ClubDetails) => void;
+  onNext: (clubDetails: ClubDetails) => void;
 }
 
 const ClubDetailForm = ({ onNext }: ClubDetailFormProps) => {
-	const navigate = useNavigate();
+  const clubNameRef = useRef<HTMLInputElement>(null);
+  const clubAcronymRef = useRef<HTMLInputElement>(null);
+  const [clubNameError, setClubNameError] = useState(false);
+  const [clubAcronymError, setClubAcronymError] = useState(false);
 
-	const clubNameRef = useRef<HTMLInputElement>(null);
-	const clubAcronymRef = useRef<HTMLInputElement>(null);
-	const [clubNameError, setClubNameError] = useState(false);
-	const [clubAcronymError, setClubAcronymError] = useState(false);
+  const [clubNameErrorMessage, setClubNameErrorMessage] =
+    useState('enter club name');
 
-	const [clubNameErrorMessage, setClubNameErrorMessage] =
-		useState('enter club name');
+  const handleOnClick = () => {
+    if (clubNameRef.current?.value === '') {
+      setClubNameErrorMessage('enter club name');
+      setClubNameError(true);
+    }
+    if (clubAcronymRef.current?.value === '') {
+      setClubAcronymError(true);
+    }
+    if (
+      clubNameRef.current?.value !== '' &&
+      clubAcronymRef.current?.value !== ''
+    ) {
+      axios
+        .post(url, {
+          clubName: clubNameRef.current?.value,
+          clubAcronym: clubAcronymRef.current?.value,
+        })
+        .then(function (response) {
+          if (response.status === 200) {
+            console.log(response.data);
+            onNext({
+              clubName: clubNameRef.current?.value!,
+              clubAcronym: clubAcronymRef.current?.value!,
+            });
+          }
+        })
+        .catch(function (error) {
+          setClubNameErrorMessage('the club you want to create already exists');
+          setClubNameError(true);
+        });
+    }
+  };
+  const handleOnBack = () => {};
 
-	const handleOnClick = () => {
-		if (clubNameRef.current?.value === '') {
-			setClubNameErrorMessage('enter club name');
-			setClubNameError(true);
-		}
-		if (clubAcronymRef.current?.value === '') {
-			setClubAcronymError(true);
-		}
-		if (
-			clubNameRef.current?.value !== '' &&
-			clubAcronymRef.current?.value !== ''
-		) {
-			axios
-				.post(url, {
-					clubName: clubNameRef.current?.value,
-					clubAcronym: clubAcronymRef.current?.value,
-				})
-				.then(function (response) {
-					if (response.status === 200) {
-						console.log(response.data);
-						onNext({
-							clubName: clubNameRef.current?.value!,
-							clubAcronym: clubAcronymRef.current?.value!,
-						});
-					}
-				})
-				.catch(function (error) {
-					setClubNameErrorMessage('the club you want to create already exists');
-					setClubNameError(true);
-				});
-		}
-	};
-	const handleOnBack = () => {
-		navigate('/no-clubs');
-	};
-
-	return (
-		<div className={styles.container}>
-			<div id={styles.backButton}>
-				<WhiteBackButton color="blue" onClick={handleOnBack} />
-			</div>
-			<h1 id={styles.title}>club details</h1>
-			<div id={styles.description}>
-				please fill out your club details to enter you club into our system
-			</div>
-			<Textfield
-				width="100%"
-				height="4rem"
-				fontSize="1.3rem"
-				placeholder="club name"
-				ref={clubNameRef}
-				onChange={() => setClubNameError(false)}
-				isError={clubNameError}
-				errorText={clubNameErrorMessage}
-			/>
-			<Textfield
-				margin="0.5rem 0 2rem 0"
-				width="100%"
-				height="4rem"
-				fontSize="1.3rem"
-				placeholder="club acronym"
-				ref={clubAcronymRef}
-				onChange={() => setClubAcronymError(false)}
-				isError={clubAcronymError}
-				errorText="enter club acronym"
-			/>
-			<Button
-				buttonText="confirm"
-				onClick={handleOnClick}
-				width="9rem"
-				height="3rem"
-				fontSize="1.2rem"
-			/>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <div id={styles.backButton}>
+        <Button
+          buttonText=""
+          icon={BackArrow}
+          onClick={handleOnBack}
+          width="55px"
+          height="55px"
+          backgroundColor="transparent"
+          border="#087DF1 solid 4px"
+          hoverColor="#cceeff"
+          iconSize="auto"
+          borderRadius="15px"
+        />
+      </div>
+      <div>
+        <div>
+          <h1 id={styles.title}>club details</h1>
+        </div>
+        <div id={styles.description}>
+          please fill out your club details to enter you club into our system
+        </div>
+      </div>
+      <div>
+        <Textfield
+          margin="2rem 2rem 2rem 2rem"
+          width="20rem"
+          height="4rem"
+          fontSize="1.5rem"
+          placeholder="club name"
+          ref={clubNameRef}
+          onChange={() => setClubNameError(false)}
+          isError={clubNameError}
+          errorText={clubNameErrorMessage}
+        />
+        <Textfield
+          margin="2rem"
+          width="20rem"
+          height="4rem"
+          fontSize="1.5rem"
+          placeholder="club acronym"
+          ref={clubAcronymRef}
+          onChange={() => setClubAcronymError(false)}
+          isError={clubAcronymError}
+          errorText="enter club acronym"
+        />
+      </div>
+      <Button
+        buttonText="confirm"
+        onClick={handleOnClick}
+        width="10rem"
+        height="4rem"
+        fontSize="1.5rem"
+      />
+    </div>
+  );
 };
 
 export default ClubDetailForm;
--- client/src/pages/no-clubs/NoClubs.tsx
+++ client/src/pages/no-clubs/NoClubs.tsx
@@ -1,57 +1,49 @@
-import Link from '../../components/Link';
-import { useState, useEffect } from 'react';
-import GoogleLogo from '../../assets/google-logo.svg';
-import styles from './style.module.css';
-import Button from '../../components/Button';
-import { useNavigate } from 'react-router';
-import axios from 'axios';
+import Link from "../../components/Link";
+import { useState, useEffect } from "react";
+import GoogleLogo from "../../assets/google-logo.svg";
+import styles from "./style.module.css";
+import Button from "../../components/Button";
+import { useNavigate } from "react-router";
+import axios from "axios";
 
 export default function NoClubs() {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const [firstName, setFirstName] = useState('friend');
-	useEffect(() => {
-		const fetchData = async () => {
-			const res = await axios({
-				url: '/firstname',
-			});
-			setFirstName(res.data.firstName);
-		};
-		fetchData();
-	}, []);
+  const [firstName, setFirstName] = useState("friend");
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios({
+        url: "/firstname",
+      });
+      setFirstName(res.data.firstName);
+    };
+    fetchData();
+  }, []);
 
-	return (
-		<div className={styles.container}>
-			<div className={styles['text-div']}>
-				<p className={styles['no-clubs']}>
-					hi {firstName}, there are currently no clubs linked to your account.
-				</p>
-				<p className={styles.subtext}>
-					please select one of the following options
-				</p>
-			</div>
-			<div className={styles['button-div']}>
-				<Button
-					width="240px"
-					height="55px"
-					buttonText="create club"
-					fontSize="1.1rem"
-					fontWeight="500"
-					backgroundColor="#03045E"
-					onClick={() => navigate('/create-club')}
-				/>
-				<Button
-					width="240px"
-					height="55px"
-					buttonText="join as club admin"
-					fontSize="1.1rem"
-					fontWeight="500"
-					backgroundColor="#087DF1"
-					color="#03045E"
-					border="0.2rem solid #03045E"
-					onClick={() => navigate('/invite-code')}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <div className={styles["text-div"]}>
+        <p className={styles["no-clubs"]}>
+          hi {firstName}, there are currently no clubs linked to your account.
+        </p>
+        <p className={styles.subtext}>
+          please select one of the following options
+        </p>
+      </div>
+      <div className={styles["button-div"]}>
+        <Button
+          buttonText="create club"
+          backgroundColor="#03045E"
+          onClick={() => navigate("/club-details")}
+        />
+        <Button
+          buttonText="join as club admin"
+          backgroundColor="#087DF1"
+          color="#03045E"
+          border="0.2rem solid #03045E"
+          onClick={() => navigate("/invite-code")}
+        />
+      </div>
+    </div>
+  );
 }
