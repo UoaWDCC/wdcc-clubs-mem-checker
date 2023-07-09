ac6fca068e210f7f9329332d701c448353a31e2c
Sebastian Thomas <stho382@aucklanduni.ac.nz>
2023-07-09 23:13:56 +1200

    finalised the first component of the logo upload

--- client/src/components/UploadButton.tsx
+++ client/src/components/UploadButton.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import styles from './UploadLogo.module.css'
+import React, { useRef } from 'react';
+import styles from './UploadLogo.module.css';
+import UploadIcon from '../assets/upload_logo.svg';
 
 interface UploadButtonProps {
     onFileSelect: (file: File) => void;
 }
 
 const UploadButton: React.FC<UploadButtonProps> = ({ onFileSelect }) => {
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    const handleButtonClick = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    };
+
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files && event.target.files[0];
         if (file && file.type.startsWith('image/')) {
@@ -15,14 +24,16 @@ const UploadButton: React.FC<UploadButtonProps> = ({ onFileSelect }) => {
 
     return (
         <div>
-            <label htmlFor="upload-button">
-                <span className={styles.buttonLabel}>choose an image to upload</span>
-            </label>
+            <button className={styles.uploadButton} onClick={handleButtonClick}>
+                <span>choose an image to upload</span>
+                <img src={UploadIcon} alt="Upload Icon" className={styles.uploadIcon} />
+            </button>
             <input
-                id= {styles["upload-button"]}
+                ref={fileInputRef}
                 type="file"
                 accept="image/*"
                 onChange={handleFileChange}
+                style={{ display: 'none' }}
             />
         </div>
     );
--- client/src/pages/create-checker-page/CustomiseLogo.tsx
+++ client/src/pages/create-checker-page/CustomiseLogo.tsx
@@ -2,6 +2,7 @@ import styles from "./style.module.css";
 import BackArrow from "../../assets/BackArrow.svg";
 import { useContext, Dispatch, SetStateAction } from "react";
 import { PageContextProvider, Page } from "./CreateCheckerPage";
+import UploadButton from "../../components/UploadButton";
 
 interface CustomiseLogoProps {
   onNext: () => void;
@@ -23,6 +24,9 @@ const CustomiseLogo = ({ onNext, onBack }: CustomiseLogoProps) => {
           <h2 className={styles.customisePageTitle}>customise page</h2>
           <i className={styles.subtitle}>customise page for your members</i>
           <p className={styles.optionalText}>please upload your club's logo (optional)</p>
+        </div>
+        <div>
+          <UploadButton/>
         </div>
         <button id={styles.CustomisePageNextButton} onClick={onNext}>
           next
