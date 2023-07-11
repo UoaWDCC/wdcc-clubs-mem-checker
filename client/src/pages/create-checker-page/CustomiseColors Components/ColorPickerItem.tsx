1ecea9b1a0a4a54d7adaa9b048ff2b14d2450b9d
Andrew Lam <bluey17501@gmail.com>
2023-07-11 21:02:39 +1200

    feat: added color picker

--- client/src/pages/create-checker-page/CustomiseColors Components/ColorPickerItem.tsx
+++ client/src/pages/create-checker-page/CustomiseColors Components/ColorPickerItem.tsx
@@ -1,15 +1,31 @@
 import { Dispatch, SetStateAction, useContext, useState } from "react";
 import ColorPickerIcon from "../../../assets/ColorPickerIcon.svg";
 import { Page, PageContextProvider } from "../CreateCheckerPage";
-import { ChromePicker } from "react-color";
+import { ChromePicker, ColorResult, RGBColor, SketchPicker } from "react-color";
 
 interface ColorPickerItemProps {}
+
+const rgbaToHex = (rgba: RGBColor): string => {
+  const { r, g, b, a } = rgba;
+  const alpha = a
+    ? Math.round(a * 255)
+        .toString(16)
+        .padStart(2, "0")
+    : "";
+  const red = r.toString(16).padStart(2, "0");
+  const green = g.toString(16).padStart(2, "0");
+  const blue = b.toString(16).padStart(2, "0");
+
+  return `#${red}${green}${blue}${alpha}`;
+};
 
 const ColorPickerItem = (props: ColorPickerItemProps) => {
   const [page, setPage] = useContext(PageContextProvider) as [
     Page,
     Dispatch<SetStateAction<Page>>
   ];
+
+  const [color, setColor] = useState<string>("#E0E0E0");
 
   const [showColorPicker, setShowColorPicker] = useState<boolean>(false);
 
@@ -20,9 +36,13 @@ const ColorPickerItem = (props: ColorPickerItemProps) => {
 
   const test = "#E0E0E0";
 
+  function handleOnChange(color: ColorResult): void {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <>
-      <div style={{ background: "red" }}>
+      <div>
         <div
           style={{
             height: "20px",
@@ -50,7 +70,6 @@ const ColorPickerItem = (props: ColorPickerItemProps) => {
         </div>
         <div
           style={{
-            background: "yellow",
             height: "20px",
             alignItems: "center",
             display: "flex",
@@ -62,11 +81,11 @@ const ColorPickerItem = (props: ColorPickerItemProps) => {
               height: "15px",
               width: "15px",
               float: "left",
-              background: page.backgroundColor ? page.backgroundColor : test,
+              background: page.backgroundColor,
             }}
           ></div>
           <div style={{ color: "black", float: "left", fontSize: "10px" }}>
-            {page.backgroundColor ? page.backgroundColor : test}
+            {page.backgroundColor}
           </div>
         </div>
       </div>
@@ -78,7 +97,13 @@ const ColorPickerItem = (props: ColorPickerItemProps) => {
             top: "10vh",
           }}
         >
-          <ChromePicker />
+          <ChromePicker
+            color={page.backgroundColor}
+            onChange={(newColor) => {
+              const newColorHex = rgbaToHex(newColor.rgb);
+              setPage({ ...page, backgroundColor: newColorHex });
+            }}
+          />
         </div>
       ) : null}
     </>
