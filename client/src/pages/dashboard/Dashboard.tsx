131fa1daaf8d28b38c6f11a5e26dfde39cb138aa
Bethany Yates <bethanyyates@wifi-172-23-77-80.uoa-wifi.auckland.ac.nz>
2023-07-19 10:43:01 +1200

    feat: created dashboard layout

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,43 @@
 import styles from './style.module.css';
+import WDCCLogo from '../../assets/WdccLogo.svg';
 
 export default function Dashboard() {
   return (
-    <div>
-      <p>Welcome to the dashboard</p>
+    <>
+    
+    
+    <div className={ styles.dashboardContainer }>
+      <img
+          className={styles.logo}
+          src={WDCCLogo}
+          alt="WDCC Logo"
+        />
+      
+      <div className={ styles.gridContainer }>
+        <div className={ styles.columnOne }>
+          <div className={ `${styles.clubDropdownContainer} ${styles.dashboardItemContainer}` }></div>
+
+          <div className={ `${styles.clubDetailsContainer} ${styles.dashboardItemContainer}` }></div>
+
+          <div className={ `${styles.usersContainer} ${styles.dashboardItemContainer}` }></div>
+        </div>
+
+        <div className={ styles.columnTwo }>
+          <div className={ `${styles.linkContainer} ${styles.dashboardItemContainer}` }></div>
+
+          <div className={ styles.columnTwoRowTwoContainer }>
+            <div className={`${styles.pagePreviewContainer} ${styles.dashboardItemContainer}`}></div>
+
+            <div className={ styles.columnTwoRowTwoColumnTwoContainer }>
+              <div className={ `${styles.adminShareContainer} ${styles.dashboardItemContainer}` }></div>
+
+              <div className={ `${styles.apiKeysContainer} ${styles.dashboardItemContainer}` }></div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
+    </>
   );
 }
