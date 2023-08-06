7cff400c88ecd09fb3af9ec4b49c7f5e2c3f7b38
Nimit Parekh <86027164+nimitoburrito@users.noreply.github.com>
2023-08-06 19:41:59 +1200

    feat: added copy button

--- client/src/components/GenerateInviteCode.tsx
+++ client/src/components/GenerateInviteCode.tsx
@@ -1,15 +1,20 @@
 import styles from './GenerateInviteCode.module.css';
 import Button from './Button';
 import axios from 'axios';
+import CopyIcon from "../assets/CopyIcon2.svg";
+import CopyTickIcon from "../assets/CopyTickIcon.svg";
+import { useState } from 'react';
 
 export interface GenerateInviteCodeProps {
-  text?: string;
+  text: string;
   onClick: () => void;
 }
 
 const GenerateInviteCode = ({
   onClick, text
+  
 }: GenerateInviteCodeProps) => {
+  const [icon, setIcon] = useState(CopyIcon);
   return (
     <>
       <div className={styles.container}>
@@ -20,6 +25,22 @@ const GenerateInviteCode = ({
         <div className={styles.subcontainer}>
           <div className={styles.text}>
             { text }
+            {text !== "click generate" && (
+            <img
+              src={icon}
+              alt="copy icon"
+              style={{
+                marginLeft: "0.5vw",
+                marginTop: "0.25vh",
+                cursor: "pointer",
+                height: "2vh",
+              }}
+              onClick={() => {
+                setIcon(CopyTickIcon);
+                navigator.clipboard.writeText(text);
+              }}
+            />
+            )}
           </div>
           <Button
             width="6vw"
