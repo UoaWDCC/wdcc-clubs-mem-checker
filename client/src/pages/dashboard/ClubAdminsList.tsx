d3e77dd9cc1fd7f971989f25721983888ff01e07
Bethany Yates <bethanyyates@wifi-172-23-80-73.uoa-wifi.auckland.ac.nz>
2023-08-12 11:46:16 +1200

    feat: created ClubAdminsList page and corresponding css file

---
+++ client/src/pages/dashboard/ClubAdminsList.tsx
@@ -0,0 +1,28 @@
+import { Dispatch, SetStateAction, useContext, useState } from "react";
+import { DashboardContextProvider, Dashboard } from "./Dashboard";
+import styles from "./ClubAdminsList.module.css"
+
+const ClubAdminsList = () => {
+  const [dashboard, setDashboard] = useContext(DashboardContextProvider) as [
+    Dashboard,
+    Dispatch<SetStateAction<Dashboard>>
+  ];
+
+  const admins = dashboard.clubAdmins || []; //Need to rename this when I find out what the name being saved to the context is
+
+  return (
+    <>
+        <div className={ styles.classListContainer }> 
+        <h1 className={ styles.classListHeader }>club admins</h1>
+        <div className={ styles.namesContainer }>
+            {admins.map((admin) => (
+                <p className={ styles.adminName }>{admin}</p>
+            ))}
+        </div>
+        </div>
+        
+    </>
+  );
+};
+
+export default ClubAdminsList;
