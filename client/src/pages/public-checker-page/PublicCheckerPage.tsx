7d8aa5c55e6d2e39c36edf397d43f751dbe2e90a
Bethany Yates <bethanyyates@wifi-172-23-87-49.uoa-wifi.auckland.ac.nz>
2023-09-04 09:25:55 +1200

    style: made page full width and height

--- client/src/pages/public-checker-page/PublicCheckerPage.tsx
+++ client/src/pages/public-checker-page/PublicCheckerPage.tsx
@@ -4,6 +4,7 @@ import Page from "../../types/Page";
 import axios from "axios";
 import { useParams } from 'react-router-dom';
 import Column from "../../types/Column";
+import styles from "./style.module.css";
 
 const PublicCheckerPage = () => {
     const [pageData, setPageData] = useState<Page>({});
@@ -23,29 +24,29 @@ const PublicCheckerPage = () => {
     console.log('page cols: ', pageData.identificationColumns);
 
     return (
-    <>
-    <ClubCheckerPage 
-        title={pageData.title}
-        backgroundColor={pageData.backgroundColor}
-        titleTextColor={pageData.titleTextColor}
-        textFieldBackgroundColor={pageData.textFieldBackgroundColor}
-        textFieldTextColor={pageData.textFieldtextColor}
-        buttonBackgroundColor={pageData.buttonColor}
-        dropDownBackgroundColor={pageData.dropDownBackgroundColor}
-        font={pageData.font}
-        clubLogoUrl={pageData.logoLink}
-        backgroundImageUrl={pageData.backgroundImageLink} 
-        optionsList={pageData.identificationColumns!}
-        
-        // optionsList={[
-        //     { originalName: "column1", displayName: "upi" },
-        //     { originalName: "column2", displayName: "first name" },
-        //     { originalName: "column3", displayName: "last name" },
-        //   ]}
-        isOnboarding={false}
-        
-        ></ClubCheckerPage>
-    </>
+    <div className={ styles.publicCheckerPageContainer }>
+        <ClubCheckerPage 
+            title={pageData.title}
+            backgroundColor={pageData.backgroundColor}
+            titleTextColor={pageData.titleTextColor}
+            textFieldBackgroundColor={pageData.textFieldBackgroundColor}
+            textFieldTextColor={pageData.textFieldtextColor}
+            buttonBackgroundColor={pageData.buttonColor}
+            dropDownBackgroundColor={pageData.dropDownBackgroundColor}
+            font={pageData.font}
+            clubLogoUrl={pageData.logoLink}
+            backgroundImageUrl={pageData.backgroundImageLink} 
+            optionsList={pageData.identificationColumns!}
+            
+            // optionsList={[
+            //     { originalName: "column1", displayName: "upi" },
+            //     { originalName: "column2", displayName: "first name" },
+            //     { originalName: "column3", displayName: "last name" },
+            //   ]}
+            isOnboarding={false}
+            
+            ></ClubCheckerPage>
+    </div>
     );
 
 }
