6d37443adcd49544624343f8a8876f1d4752d2e4
Jordan Wen Jie See <wsee046@aucklanduni.ac.nz>
2023-10-02 18:34:35 +1300

    refactor: remove unused DashboardPage.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,84 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-} from 'react-router-dom';
-import { HomePage } from './pages/home/home';
-import CreateCheckerPage from './pages/create-checker-page/CreateCheckerPage';
-import ClubDetailPage from './pages/club-detail-page/ClubDetailPage';
-import { GoogleSignIn } from './pages/google-sign-in/GoogleSignInPage';
-import NoClubs from './pages/no-clubs/NoClubs';
-import Dashboard from './pages/dashboard/Dashboard';
-import GoogleCallback from './pages/google-callback/GoogleCallback';
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import InviteCodePage from './pages/invite-code/InviteCodePage';
-import { ConfimationPage } from './pages/onboarding-confirmation-page/ConfirmationPage';
-import ClubCheckerPage from './pages/club-checker-page/ClubCheckerPage';
-import EmptyClubLogo from './assets/EmptyClubLogo.svg';
-import PublicCheckerPage from './pages/public-checker-page/PublicCheckerPage';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { HomePage } from "./pages/home/home";
+import CreateCheckerPage from "./pages/create-checker-page/CreateCheckerPage";
+import ClubDetailPage from "./pages/club-detail-page/ClubDetailPage";
+import { GoogleSignIn } from "./pages/google-sign-in/GoogleSignInPage";
+import NoClubs from "./pages/no-clubs/NoClubs";
+import Dashboard from "./pages/dashboard/Dashboard";
+import GoogleCallback from "./pages/google-callback/GoogleCallback";
+import axios from "axios";
+import Cookies from "js-cookie";
+import InviteCodePage from "./pages/invite-code/InviteCodePage";
+import { ConfimationPage } from "./pages/onboarding-confirmation-page/ConfirmationPage";
+import PublicCheckerPage from "./pages/public-checker-page/PublicCheckerPage";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <HomePage />,
   },
   {
-    path: '/create-page',
+    path: "/create-page",
     element: <CreateCheckerPage />,
   },
   {
-    path: '/sign-in',
+    path: "/sign-in",
     element: <GoogleSignIn />,
   },
   {
-    path: '/no-clubs',
+    path: "/no-clubs",
     element: <NoClubs />,
   },
   {
-    path: '/dashboard',
+    path: "/dashboard",
     element: <Dashboard />,
   },
   {
-    path: '/auth/google/callback',
+    path: "/auth/google/callback",
     element: <GoogleCallback />,
   },
   {
-    path: '/club-details',
+    path: "/club-details",
     element: <ClubDetailPage />,
   },
   {
-    path: '/invite-code',
+    path: "/invite-code",
     element: <InviteCodePage />,
   },
   {
-    path: '/confirmation',
+    path: "/confirmation",
     element: <ConfimationPage />,
   },
   {
-    path: '/:webLinkId',
+    path: "/:webLinkId",
     element: <PublicCheckerPage />,
   },
 ]);
 
 // Find the auth token in local storage if it exists
-const token: string | undefined = Cookies.get('token');
+const token: string | undefined = Cookies.get("token");
 if (token != undefined) {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 }
 
-if (import.meta.env.MODE == 'production') {
-  axios.defaults.baseURL = '/api/';
+if (import.meta.env.MODE == "production") {
+  axios.defaults.baseURL = "/api/";
 } else {
-  axios.defaults.baseURL = 'http://localhost:3000/api/';
+  axios.defaults.baseURL = "http://localhost:3000/api/";
 }
 
 function hasClubs(): boolean {
   axios
-    .get('/user/organisations')
+    .get("/user/organisations")
     .then((res) => {
       if (res.status === 204) {
         return true;
@@ -88,6 +81,6 @@ function hasClubs(): boolean {
   return false;
 }
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <RouterProvider router={router} />
 );
