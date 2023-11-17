267bc913064537863b1f8fc0bcafb5f1187e2849
Andrew Lam <bluey17501@gmail.com>
2023-11-17 15:33:51 +1300

    refactor: updated context for CustomiseColors

--- client/src/pages/create-checker-page/CustomiseColors.tsx
+++ client/src/pages/create-checker-page/CustomiseColors.tsx
@@ -1,12 +1,13 @@
-import styles from './style.module.css';
-import Button from '../../components/Button';
-import BackButton from '../../components/BackButton';
-import { useContext, Dispatch, SetStateAction } from 'react';
-import { PageContextProvider } from './CreateCheckerPage';
-import IPage from '../../types/IPage';
-import { ColorPicker } from './CustomiseColors Components/ColorPicker';
-import { ChromePicker } from 'react-color';
-import ClubCheckerPage from '../club-checker-page/ClubCheckerPage';
+import styles from "./style.module.css";
+import Button from "../../components/Button";
+import BackButton from "../../components/BackButton";
+import { useContext, Dispatch, SetStateAction } from "react";
+import { PageContextProvider } from "./CreateCheckerPage";
+import IPage from "../../types/IPage";
+import { ColorPicker } from "./CustomiseColors Components/ColorPicker";
+import { ChromePicker } from "react-color";
+import ClubCheckerPage from "../club-checker-page/ClubCheckerPage";
+import ICreateCheckerPageContext from "../../types/ICreateCheckerPageContext";
 
 interface CustomiseColorsProps {
   onNext: () => void;
@@ -14,12 +15,12 @@ interface CustomiseColorsProps {
 }
 
 const CustomiseColors = ({ onNext, onBack }: CustomiseColorsProps) => {
-  const [page, setPage] = useContext(PageContextProvider) as [
-    IPage,
-    Dispatch<SetStateAction<IPage>>
+  const [context, setContext] = useContext(PageContextProvider) as [
+    ICreateCheckerPageContext,
+    Dispatch<SetStateAction<ICreateCheckerPageContext>>
   ];
   function handleNext(): void {
-    throw new Error('Function not implemented.');
+    throw new Error("Function not implemented.");
   }
 
   return (
@@ -38,13 +39,10 @@ const CustomiseColors = ({ onNext, onBack }: CustomiseColorsProps) => {
         <div className={styles.title}>
           <h1>customise page</h1>
         </div>
-        <i
-          className={styles.subtitle}
-          style={{ fontWeight: 500 }}
-        >
+        <i className={styles.subtitle} style={{ fontWeight: 500 }}>
           customise page for your members
         </i>
-        <div style={{ float: 'left', width: '100%' }}>
+        <div style={{ float: "left", width: "100%" }}>
           <ColorPicker />
         </div>
         <div id={styles.CustomisePageNextButton}>
@@ -60,26 +58,28 @@ const CustomiseColors = ({ onNext, onBack }: CustomiseColorsProps) => {
         <div className={styles.preview}>
           <ClubCheckerPage
             clubId={0}
-            clubName={''}
-            title={page.title}
-            backgroundColor={page.backgroundColor}
-            titleTextColor={page.titleTextColor}
-            textFieldBackgroundColor={page.textFieldBackgroundColor}
-            textFieldTextColor={page.textFieldtextColor}
-            buttonBackgroundColor={page.buttonColor}
-            dropDownBackgroundColor={page.dropDownBackgroundColor}
-            font={page.font}
+            clubName={""}
+            title={context.page.title}
+            backgroundColor={context.page.backgroundColor}
+            titleTextColor={context.page.titleTextColor}
+            textFieldBackgroundColor={context.page.textFieldBackgroundColor}
+            textFieldTextColor={context.page.textFieldtextColor}
+            buttonBackgroundColor={context.page.buttonColor}
+            dropDownBackgroundColor={context.page.dropDownBackgroundColor}
+            font={context.page.font}
             clubLogoUrl={
-              // @ts-ignore
-              page.logoLink ? URL.createObjectURL(page.logoLink!) : undefined
-            }
-            backgroundImageUrl={
-              page.backgroundImageLink
+              context.page.logoLink
                 ? // @ts-ignore
-                  URL.createObjectURL(page.backgroundImageLink!)
+                  URL.createObjectURL(context.page.logoLink!)
                 : undefined
             }
-            optionsList={page.identificationColumns || []}
+            backgroundImageUrl={
+              context.page.backgroundImageLink
+                ? // @ts-ignore
+                  URL.createObjectURL(context.page.backgroundImageLink!)
+                : undefined
+            }
+            optionsList={context.page.identificationColumns || []}
             isOnboarding={true}
           />
         </div>
