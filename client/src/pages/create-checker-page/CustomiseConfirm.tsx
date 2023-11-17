bfa40d5bbadebcbbd025a35c1f411c434b9e62c5
Andrew Lam <bluey17501@gmail.com>
2023-11-17 15:34:17 +1300

    refactro: updated context for CustomiseConfirm

--- client/src/pages/create-checker-page/CustomiseConfirm.tsx
+++ client/src/pages/create-checker-page/CustomiseConfirm.tsx
@@ -10,6 +10,7 @@ import ClubCheckerPage from "../club-checker-page/ClubCheckerPage";
 import { getSpreadsheetId } from "./GoogleSheetForm";
 import { IClubDetails } from "../club-detail-page/ClubDetailPage";
 import { getSheetTabId } from "./GoogleSheetForm";
+import ICreateCheckerPageContext from "../../types/ICreateCheckerPageContext";
 
 interface CustomiseConfirmProps {
   onNext: () => void;
@@ -22,9 +23,9 @@ const CustomiseConfirm = ({
   onBack,
   clubDetails,
 }: CustomiseConfirmProps) => {
-  const [page, setPage] = useContext(PageContextProvider) as [
-    IPage,
-    Dispatch<SetStateAction<IPage>>
+  const [context, setConetxt] = useContext(PageContextProvider) as [
+    ICreateCheckerPageContext,
+    Dispatch<SetStateAction<ICreateCheckerPageContext>>
   ];
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -32,35 +33,42 @@ const CustomiseConfirm = ({
     axios(`/club/get-organisationId/${clubDetails.clubName}`).then((res) => {
       const id = res.data.organisationId;
       const formData = new FormData();
-      if (page.backgroundImageLink)
-        formData.append("background", page.backgroundImageLink);
-      if (page.logoLink) formData.append("logo", page.logoLink);
-      if (page.title) formData.append("name", page.title);
+      if (context.page.backgroundImageLink)
+        formData.append("background", context.page.backgroundImageLink);
+      if (context.page.logoLink) formData.append("logo", context.page.logoLink);
+      if (context.page.title) formData.append("name", context.page.title);
       formData.append("organisationId", id);
       formData.append(
         "identificationColumns",
-        JSON.stringify(page.identificationColumns)
+        JSON.stringify(context.page.identificationColumns)
       );
-      formData.append("sheetId", getSpreadsheetId(page.googleSheetLink!)!);
-      formData.append("sheetTabId", getSheetTabId(page.googleSheetLink!)!);
+      formData.append(
+        "sheetId",
+        getSpreadsheetId(context.page.googleSheetLink!)!
+      );
+      formData.append(
+        "sheetTabId",
+        getSheetTabId(context.page.googleSheetLink!)!
+      );
       formData.append(
         "textFieldBackgroundColor",
-        page.textFieldBackgroundColor!
+        context.page.textFieldBackgroundColor!
       );
-      if (page.textFieldtextColor)
-        formData.append("textColor", page.textFieldtextColor!);
-      if (page.buttonColor) formData.append("buttonColor", page.buttonColor!);
-      if (page.titleTextColor)
-        formData.append("headingColor", page.titleTextColor!);
-      if (page.font) formData.append("fontFamily", page.font!);
-      if (page.backgroundColor)
-        formData.append("backgroundColor", page.backgroundColor);
-      if (page.dropDownBackgroundColor)
+      if (context.page.textFieldtextColor)
+        formData.append("textColor", context.page.textFieldtextColor!);
+      if (context.page.buttonColor)
+        formData.append("buttonColor", context.page.buttonColor!);
+      if (context.page.titleTextColor)
+        formData.append("headingColor", context.page.titleTextColor!);
+      if (context.page.font) formData.append("fontFamily", context.page.font!);
+      if (context.page.backgroundColor)
+        formData.append("backgroundColor", context.page.backgroundColor);
+      if (context.page.dropDownBackgroundColor)
         formData.append(
           "dropDownBackgroundColor",
-          page.dropDownBackgroundColor
+          context.page.dropDownBackgroundColor
         );
-      console.log("confirm:" + page.dropDownBackgroundColor);
+      console.log("confirm:" + context.page.dropDownBackgroundColor);
       axios
         .post("/pages/create", formData)
         .then((res) => {
@@ -131,25 +139,27 @@ const CustomiseConfirm = ({
           <ClubCheckerPage
             clubId={0}
             clubName={""}
-            title={page.title}
-            backgroundColor={page.backgroundColor}
-            titleTextColor={page.titleTextColor}
-            textFieldBackgroundColor={page.textFieldBackgroundColor}
-            textFieldTextColor={page.textFieldtextColor}
-            buttonBackgroundColor={page.buttonColor}
-            dropDownBackgroundColor={page.dropDownBackgroundColor}
-            font={page.font}
+            title={context.page.title}
+            backgroundColor={context.page.backgroundColor}
+            titleTextColor={context.page.titleTextColor}
+            textFieldBackgroundColor={context.page.textFieldBackgroundColor}
+            textFieldTextColor={context.page.textFieldtextColor}
+            buttonBackgroundColor={context.page.buttonColor}
+            dropDownBackgroundColor={context.page.dropDownBackgroundColor}
+            font={context.page.font}
             clubLogoUrl={
-              // @ts-ignore
-              page.logoLink ? URL.createObjectURL(page.logoLink!) : undefined
-            }
-            backgroundImageUrl={
-              page.backgroundImageLink
+              context.page.logoLink
                 ? // @ts-ignore
-                  URL.createObjectURL(page.backgroundImageLink!)
+                  URL.createObjectURL(context.page.logoLink!)
                 : undefined
             }
-            optionsList={page.identificationColumns || []}
+            backgroundImageUrl={
+              context.page.backgroundImageLink
+                ? // @ts-ignore
+                  URL.createObjectURL(context.page.backgroundImageLink!)
+                : undefined
+            }
+            optionsList={context.page.identificationColumns || []}
             isOnboarding={true}
           />
         </div>
